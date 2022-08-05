53e19bee4fb5150de451d6b63be209230edc8f11
zizpzn <zizpzn@gmail.com>
2022-08-05 11:54:18 +0700

    get most popular list API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import VideoList from "./components/video_list/VideoList";
 
 function App() {
-  return <h1>Hello React</h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyD5PE2WWsgT2UMXlyCT4GxWB_baAHfVcK0",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  return <VideoList videos={videos} />;
 }
 
 export default App;
