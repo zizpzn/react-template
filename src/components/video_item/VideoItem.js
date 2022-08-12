b460c2f3a20caf3b972fc020796bbb6e05a81be7
zizpzn <zizpzn@gmail.com>
2022-08-12 11:04:45 +0700

    video detail part 1

--- src/components/video_item/VideoItem.js
+++ src/components/video_item/VideoItem.js
@@ -1,20 +1,26 @@
 import React from "react";
 import styles from "./VideoItem.module.css";
 
-const VideoItem = ({ video, video: { snippet }, onVideoClick }) => (
-  <li className={styles.container} onClick={() => onVideoClick(video)}>
-    <div className={styles.video}>
-      <img
-        className={styles.thumbnail}
-        src={snippet.thumbnails.medium.url}
-        alt="video thumbnail"
-      />
-      <div className={styles.metadata}>
-        <p className={styles.title}>{snippet.title}</p>
-        <p className={styles.channel}>{snippet.channelTitle}</p>
+const VideoItem = ({ video, video: { snippet }, onVideoClick, display }) => {
+  const displayType = display === "list" ? styles.list : styles.grid;
+  return (
+    <li
+      className={`${styles.container} ${displayType}`}
+      onClick={() => onVideoClick(video)}
+    >
+      <div className={styles.video}>
+        <img
+          className={styles.thumbnail}
+          src={snippet.thumbnails.medium.url}
+          alt="video thumbnail"
+        />
+        <div className={styles.metadata}>
+          <p className={styles.title}>{snippet.title}</p>
+          <p className={styles.channel}>{snippet.channelTitle}</p>
+        </div>
       </div>
-    </div>
-  </li>
-);
+    </li>
+  );
+};
 
 export default VideoItem;
--- src/components/video_list/VideoList.js
+++ src/components/video_list/VideoList.js
@@ -2,10 +2,15 @@ import React from "react";
 import VideoItem from "../video_item/VideoItem";
 import styles from "./VideoList.module.css";
 
-const VideoList = ({ videos, onVideoClick }) => (
+const VideoList = ({ videos, onVideoClick, display }) => (
   <ul className={styles.videos}>
     {videos.map((video) => (
-      <VideoItem key={video.id} video={video} onVideoClick={onVideoClick} />
+      <VideoItem
+        key={video.id}
+        video={video}
+        onVideoClick={onVideoClick}
+        display={display}
+      />
     ))}
   </ul>
 );
